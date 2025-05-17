194cdea3b9c6c12a8df098198a8461f21ba89256
Andrea Ferenczova <andrea.ferenczova@student.tuke.sk>
2025-05-17 15:21:27 +0200

    Update ghpages

--- assets/javascript-B_WBOd7V.js
+++ assets/javascript-B_WBOd7V.js
@@ -1,4 +1,4 @@
-import{conf as t,language as e}from"./typescript-CcKDdyQw.js";import"./index-CXTIgGjr.js";/*!-----------------------------------------------------------------------------
+import{conf as t,language as e}from"./typescript-eWa8PgMt.js";import"./index-B69nQy6b.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.52.2(404545bded1df6ffa41ea0af4e8ddb219018c6c1)
  * Released under the MIT license
